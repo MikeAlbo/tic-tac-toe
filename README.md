a30971f3efe3eb73b69699c540281ab0ae97fc86
Michael Albonetti <malbo24@gmail.com>
2016-12-18 11:19:00 -0600

    build game setup questions

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # Tic Tac Toe
 ReadMe file
+
+## js 
+
+// game logic
+
+// initalize the game state
+// minimax
+// update html score
+// take user input
+//alert popup from bottom winner or looser // ai smack talking maybe too
+// in code, user will be 1 computer will be 0 (true, false)
+
+//game play
+// user selects player
+// user decides who goes first (if !score)
+// user turn --
+// user clicks on tile, onclick calls function to add user's marker to tile
+// function checks that tile is MT, adds the players marker (1), calls win function
+// win function, if totalCount > 5, checks to see if there is a winning row -- takes in the current player
+/* 
+        if(totalCount > 5){
+            if(
+                (board[0] == player && board[1] == player && board[2] == player)
+                ...
+            )
+        }
+*/
+// if win, call endGame function
+// function endGame, updates score, modifies tile css
+
+// if noWin, totalCount++, if totalCount == 9, call draw, modify css, else ( if user turn, wait for input, else call AI )
+
+// AI 
+// basicly, captures the current gameboard and runs a recursive function to test the number of moves until completion 
+
+// AI takes in the current state of the game board
+
+/* 
+        x,o, ,
+        o,x,x,
+        x, , 
+*/
+
+// ... and checks the availabe spaces for the optimal decision
+
+/*
+    recursion( currentState, player) {
+        var currentState = currentState;
+        var levelCount = 0;
+        
+        function checkLevel(state) {
+            var thisLevelCount = 0;
+            for(var i = 0; i < 9; i++ ){
+                if (!state[i]) {
+                    state[i] = player;
+                    if(DidWin(state) && thisLevelCount < 9){
+                        return state;
+                    } else {
+                        thisLevelCount++;
+                        checkLevel(state);
+                    }
+                    
+                }
+            }
+            
+            if // count is less than 9 return winner
+            if count  == 9, no winner return draw
+            if this levelcount  > thisLevelCount return levelCount == thisLevelCount
+            
+        }
+        
+    }
+*/
+
+//user can clear the total scores
--- js/gameLogic.js
+++ js/gameLogic.js
@@ -1,1 +1,120 @@
-// game logic
+$(document).ready(function(){
+    
+    var player = { peice: "x" };
+    
+    var gameTurn = 1;
+    
+    //container gameView div
+    var gameView = $("#gameView");
+    
+    // init the startup screen
+    initStartupScreen();
+    
+    //function to init the screen with title and start message
+    
+    function initStartupScreen(){
+        gameView.html('<div id="openSeqContainer" class="text-center jumbotron"></div>');
+        
+        var container = $("#openSeqContainer");
+        container.html('<div id="titleContainer"><div>');
+        
+        var titleContainer = $("#titleContainer");
+
+        var title = '<h1>Tic Tac Toe<br><small>you against the machine...</small></h1>';
+        var button = '<button class="btn btn-lg btn-primary" id="startGameButton">Start</button>';
+        titleContainer.html(title + button);
+        
+        startGameSetup(container, titleContainer);
+   
+    }
+    
+    //event function to listen for "start" (make can make a general event listener with params)
+    
+    function startGameSetup(container, titleContainer){
+        $("#startGameButton").on("click", function(){
+            clearScreen(container, titleContainer);
+            selectGamePeice(container);    
+        });
+        
+        function clearScreen(container, titleContainer){  // mod to animation
+            container.html(''); // clear after animation
+            titleContainer.html(''); // change to animate out
+        }
+        
+        function selectGamePeice(container){
+            
+            container.html('<div id="gamePeiceContainer"></div>');
+            var gamePeiceContainer = $("#gamePeiceContainer");
+            var question = '<h3>Select your game peice.</h3>';
+            var button = '<div class="btn-group" role="group" aria-label="..."><button type="button" class="btn btn-default" id="selectX"> &nbsp; X &nbsp; </button><button type="button" class="btn btn-default" id="selectO">&nbsp; O &nbsp;</button></div>';
+            
+            gamePeiceContainer.html(question + button);
+            
+            $("#selectX").on("click", function(){player.peice = "x"; selectTurn(container, gamePeiceContainer);});
+            $("#selectO").on("click", function(){player.peice = "o"; selectTurn(container, gamePeiceContainer);});
+            
+            
+            
+        } // select game peice
+        
+        function selectTurn(container, gamePeiceContainer){
+            
+            clearGamePeiceScreen(container, gamePeiceContainer);
+            askForTurn(container);
+            
+            //clear gamePeiceContainer
+            function clearGamePeiceScreen(container, gamePeiceContainer, turnContainer){ // animation
+                gamePeiceContainer.html(''); // animate out
+                
+                container.html(''); //clear after animation
+                
+            } //clear
+            
+            //ask for turn
+            function askForTurn(container){
+                
+                container.html('<div id="turnContainer"></div>');
+                var turnContainer = $("#turnContainer");
+                
+                var title = '<h3>Who Should Start?</h3>';
+                var button = '<div class="btn-group" role="group" aria-label="..."><button type="button" class="btn btn-default" id="selectMe"> &nbsp; Me &nbsp; </button><button type="button" class="btn btn-default" id="selectComputer">&nbsp; Computer &nbsp;</button></div>';
+                
+                turnContainer.html(title + button);
+                
+                $("#selectMe").on("click", function(){gameTurn = 1; initGame();});
+                $("#selectComputer").on("click", function(){gameTurn = 0; initGame();});
+                
+                function initGame(){
+                    clearGamePeiceScreen(container,gamePeiceContainer, turnContainer);
+                }
+                
+                
+            } // ask for turn
+            
+        } // select turn
+        
+    } // start game setup
+    
+    
+    
+    // function to ask user to select player
+    
+    //function to ask user if they'd like to go first
+    
+    //function to init the start sequence upon "doc.ready"
+    
+    //function to build gameboard
+    
+    //function to build tile
+    
+    //function to add tiles to gameboard
+    
+    //function to call gameboard setup
+   
+    
+    
+}); // document ready
+
+
+
+
